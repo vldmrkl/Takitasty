780f838062571cac2b78877c2bb34bf6c2de1548
Volodymyr <klymenkodp@gmail.com>
2019-10-30 22:50:08 -0400

    Initial Commit

---
+++ Takitasty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Takitasty
+//
+//  Created by Volodymyr Klymenko on 2019-10-30.
+//  Copyright © 2019 Volodymyr Klymenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
