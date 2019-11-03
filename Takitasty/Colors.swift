8eae23b05f047171ea7975b5bc9060c45f1352e7
Volodymyr <klymenkodp@gmail.com>
2019-11-03 18:45:42 -0500

    Create Colors structure

---
+++ Takitasty/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  Takitasty
+//
+//  Created by Volodymyr Klymenko on 2019-11-03.
+//  Copyright © 2019 Volodymyr Klymenko. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Colors {
+    static let blue             = UIColor(red: 74.0/255.0, green: 144.0/255.0, blue: 228.0/255.0, alpha: 1.0)
+    static let lightBlue        = UIColor(red: 0.0/255.0, green: 210.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+    static let darkBlue         = UIColor(red: 58.0/255.0, green: 123.0/255.0, blue: 213.0/255.0, alpha: 1.0)
+}
