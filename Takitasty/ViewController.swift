3882e7a470427ff4532d3b2f187b0425cc5c0d96
Volodymyr <klymenkodp@gmail.com>
2019-11-03 18:47:04 -0500

    Implement initial start screen UI

--- Takitasty/ViewController.swift
+++ Takitasty/ViewController.swift
@@ -9,12 +9,125 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let city = "Toronto"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpLayout()
     }
 
 
-}
+    func setUpLayout() {
+        if let bgImage = UIImage(named: "food-bg") {
+            let imageView = UIImageView(frame: self.view.bounds);
 
+            imageView.image = bgImage
+            imageView.contentMode = .scaleAspectFill
+            self.view.addSubview(imageView)
+
+            view.setGradientBackground(colorOne: Colors.lightBlue, colorTwo: Colors.darkBlue, opacity: 0.9)
+            self.view.sendSubviewToBack(imageView)
+        }
+
+
+        let mainStackView = UIStackView()
+        mainStackView.axis = .vertical
+        mainStackView.alignment = .center
+        mainStackView.spacing = 100
+
+        let logoStackView = UIStackView()
+        logoStackView.axis = .vertical
+        logoStackView.alignment = .center
+        logoStackView.distribution = .equalSpacing
+        logoStackView.spacing = 10
+
+
+        if let logoImage = UIImage(named: "logo") {
+            let logoImageView = UIImageView(image: logoImage)
+            logoImageView.widthAnchor.constraint(equalToConstant: 250).isActive = true
+            logoImageView.heightAnchor.constraint(equalToConstant: 130).isActive = true
+            logoImageView.contentMode = .scaleAspectFit
+            logoStackView.addArrangedSubview(logoImageView)
+        }
+
+        let nameLabel = UILabel()
+        nameLabel.text = "takitasty".uppercased()
+        nameLabel.textColor = .white
+        nameLabel.font = UIFont(name: "Futura-Medium", size: 36.0)
+        logoStackView.addArrangedSubview(nameLabel)
+
+        mainStackView.addArrangedSubview(logoStackView)
+
+        let buttonsStackView = UIStackView()
+        buttonsStackView.axis = .vertical
+        buttonsStackView.alignment = .center
+        buttonsStackView.distribution = .equalSpacing
+        buttonsStackView.spacing = 10
+
+        buttonsStackView.addArrangedSubview(createMenuButton(title: "All Restaurants", action: nil))
+        buttonsStackView.addArrangedSubview(createMenuButton(title: "Restaurants By Cuisine", action: nil))
+        buttonsStackView.addArrangedSubview(createMenuButton(title: "I'm Feeling Lucky", action: nil))
+
+        mainStackView.addArrangedSubview(buttonsStackView)
+
+        let locationStackView = UIStackView()
+        locationStackView.axis = .horizontal
+        locationStackView.alignment = .center
+        locationStackView.distribution = .equalSpacing
+        locationStackView.spacing = 5
+
+        if let locationIcon = UIImage(systemName: "location.fill") {
+
+            let locationIconImageView = UIImageView(image: locationIcon.withTintColor(.white, renderingMode: .alwaysOriginal))
+            locationIconImageView.widthAnchor.constraint(equalToConstant: 14.0).isActive = true
+            locationIconImageView.heightAnchor.constraint(equalToConstant: 14.0).isActive = true
+
+            locationIconImageView.contentMode = .scaleAspectFit
+            locationStackView.addArrangedSubview(locationIconImageView)
+        }
+
+        let locationLabel = UILabel()
+        locationLabel.text = "\(city)".uppercased()
+        locationLabel.textColor = .white
+        locationLabel.font = UIFont(name: "Futura-Medium", size: 14.0)
+        locationStackView.addArrangedSubview(locationLabel)
+        locationStackView.layoutMargins.top = 300
+
+
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(mainStackView)
+        mainStackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        mainStackView.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor).isActive = true
+
+
+        locationStackView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(locationStackView)
+        locationStackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        locationStackView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+
+    }
+
+    func createMenuButton(title: String, action: Selector?) -> UIButton {
+        let menuButton = UIButton(frame: CGRect(x: 0, y: 0, width: 300, height: 50))
+        menuButton.translatesAutoresizingMaskIntoConstraints = false
+        menuButton.layer.cornerRadius = 10.0
+        menuButton.layer.masksToBounds = true
+        menuButton.setTitle(title, for: .normal)
+        menuButton.titleLabel?.font = UIFont(name: "Avenir-Medium", size: 20.0)
+        if let action = action {
+            menuButton.addTarget(self, action: action, for: .touchUpInside)
+        }
+        menuButton.heightAnchor.constraint(equalToConstant: 50.0).isActive = true
+        menuButton.widthAnchor.constraint(equalToConstant: 250).isActive = true
+
+        return menuButton
+    }
+
+    @objc func testFunc(sender: UIButton){
+        print("kaboom")
+    }
+
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+}
