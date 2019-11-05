bd26b69f317ebda2b63ed4f17510cde893773d41
Volodymyr <klymenkodp@gmail.com>
2019-11-05 00:51:12 -0500

    Implement conditional launching screen

--- Takitasty/ViewController.swift
+++ Takitasty/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpLayout()
+        UserDefaults.standard.set(0, forKey: "lat")
+        UserDefaults.standard.set(0, forKey: "lon")
     }
 
 
